58053f8bb70987a39cbaf82de0e62858af3ac445
osasai <osasaito@gmail.com>
2014-10-07 12:55:13 +0100

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:email].downcase)
 		if user && user.authenticate(params[:password])
 			sign_in user
-			redirect_to user
-			# ユーザーをサインインさせ、ユーザーページ(show)にリダイレクトする。
+			redirect_back_or user
 		else
 			# エラーメッセージを表示し、サインインフォームを再描画する
 			flash.now[:error] = "Invalid email/password combination"
